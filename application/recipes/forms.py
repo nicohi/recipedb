877272cd87d9a3472c6391b8eda5a0e31a379dcd
Nicolas Hiillos <nicolashiillos@gmail.com>
2020-04-02 15:46:27 +0300

    Add initial support for ingredients and tools

--- application/recipes/forms.py
+++ application/recipes/forms.py
@@ -1,12 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SelectMultipleField, validators
 
+ingredients = [('0', 'Cheese'),
+                ('1', 'Eggs'),
+                ('3', 'Milk'),
+                ('2', 'Flour')]
+
+tools = [('0', 'Grill'),
+                ('1', 'Microwave'),
+                ('3', 'Pan'),
+                ('2', 'Pot')]
+
 class RecipeForm(FlaskForm):
     name = StringField("Recipe name", [validators.Length(min=3)])
     favourite = BooleanField("Favourite")
 
-    ingredients = SelectMultipleField(u'Ingredients', choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')])
-    tools = SelectMultipleField(u'Tools', choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')])
+    ingredients = SelectMultipleField(u'Ingredients', choices=ingredients)
+    tools = SelectMultipleField(u'Tools', choices=tools)
 
     text = StringField("Recipe text")
  
--- application/views.py
+++ application/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from application import app
+from application.recipes.models import Recipe
   
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", favs=Recipe.find_favourites())
